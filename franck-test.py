e925f41361019723cc9439833ea346cd631afae2
Franck Vervial <franck.vervial@lazada.com>
2017-06-23 08:56:18 +0800

    created franck-test.py and update data_processing_templace-FV.py

---
+++ franck-test.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun 23 07:35:36 2017
+
+@author: franck.vervial
+"""
+import numpy as np
+#test column
+X = np.zeros((3, 3))
+M = np.matrix('1 2 3; 4 5 6; 7 8 9')
+M[0][0]
+
+tableau = np.array([[1, 2, 3],
+                    [4, 5, 6],
+                    [7, 8, 9]])
+
+a = [1,2,3]
